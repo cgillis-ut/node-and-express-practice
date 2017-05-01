00dcb4bcec489f1c8d571925f3746567b5429cae
christinegillis <chrisa8300@gmail.com>
2017-04-30 20:32:39 -0400

    adding wip

---
+++ meadowlark.js
@@ -0,0 +1,55 @@
+var express = require('express'); 
+var app = express(); 
+
+// set up handlebars view engine 
+var handlebars = require('express-handlebars')
+	.create({ defaultLayout:'main' }); 
+
+
+app.engine('handlebars', handlebars.engine); 
+app.set('view engine', 'handlebars'); 
+
+app.set('port', process.env.PORT || 3000); 
+
+app.use(express.static(__dirname + '/public'));
+
+var fortuneCookies = [
+	"Conquer your fears or they will conquer you.",
+	"Rivers need springs.",
+	"Do not fear what you don't know.",
+	"You will have a pleasant surprise.",
+	"Whenever possible, keep it simple.",
+];
+
+//create routes for html pgs
+app.get('/', function(req, res){
+	res.render('home');
+});
+
+app.get('/about', function(req, res){
+	var randomFortune = 
+		fortuneCookies[Math.floor(Math.random() * fortuneCookies.length)];
+	res.render("about", {fortune: randomFortune}
+		);
+
+}); 
+
+
+
+// custom 404 page 
+app.use(function(req, res, next){
+	res.status(404);
+	res.render('404');
+}); 
+
+//500 page
+app.use(function(req, res){
+	console.error(err.stack);
+	res.status(500);
+	res.render("500");
+});
+
+app.listen(
+	app.get('port'), function(){
+		console.log( 'Express started on http://localhost:' +app.get('port') + '; press Ctrl-C to terminate.' ); 
+}); 
